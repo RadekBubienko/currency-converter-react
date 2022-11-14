1a25dc5b8803f60ae770dada0e206e304f27ef31
Radek Bubienko <radek@bubienko.pl>
2022-11-14 17:48:56 +0100

    created hook in component Clock

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,22 +1,12 @@
-import CurrentDate from "../CurrentDate";
 import { ClockContainer } from "./styled";
+import { useCurrentDate } from "./useCurrentDate";
 
 const Clock = () => {
 
-  const { myDate } = CurrentDate();
-
-  const dateRender = myDate.toLocaleDateString(undefined, {
-    month: "long",
-    weekday: "long",
-    day: "numeric",
-    year: "numeric",
-  });
-
-  const timeRender = myDate.toLocaleTimeString(undefined, {
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-  });
+  const {
+    dateRender,
+    timeRender,
+  } = useCurrentDate();
 
   return (
     <>
---
+++ src/Clock/useCurrentDate.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+export const useCurrentDate = () => {
+    const [myDate, setMyDate] = useState(new Date());
+
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setMyDate(new Date())
+      }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+        };
+    }, []);
+
+    const dateRender = myDate.toLocaleDateString(undefined, {
+        month: "long",
+        weekday: "long",
+        day: "numeric",
+        year: "numeric",
+      });
+    
+      const timeRender = myDate.toLocaleTimeString(undefined, {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      });
+
+    return { 
+        myDate,
+        dateRender,
+        timeRender,
+    };
+  };
